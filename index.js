9a42c8dc73903d3da9fc532bc2cb7d6c870174af
TheRainbowCraft5707 <82901383+TheRainbowCraft5707@users.noreply.github.com>
2022-01-16 21:16:40 +0100

    Update

--- index.js
+++ index.js
@@ -25,6 +25,34 @@ client.on('available_commands', (packet) => {
                     if (parameter.enum_type == "soft_enum")
                         parameter.enum_type = 1024;
                 }
+                if (parameter.value_type) {
+                    if (parameter.value_type == "int")
+                        parameter.value_type = 1;
+                    if (parameter.value_type == "float")
+                        parameter.value_type = 2;
+                    if (parameter.value_type == "value")
+                        parameter.value_type = 3;
+                    if (parameter.value_type == "wildcard_int")
+                        parameter.value_type = 4;
+                    if (parameter.value_type == "operator")
+                        parameter.value_type = 5;
+                    if (parameter.value_type == "target")
+                        parameter.value_type = 6;
+                    if (parameter.value_type == "file_path")
+                        parameter.value_type = 16;
+                    if (parameter.value_type == "string")
+                        parameter.value_type = 32;
+                    if (parameter.value_type == "position")
+                        parameter.value_type = 40;
+                    if (parameter.value_type == "message")
+                        parameter.value_type = 44;
+                    if (parameter.value_type == "raw_text")
+                        parameter.value_type = 46;
+                    if (parameter.value_type == "json")
+                        parameter.value_type = 50;
+                    if (parameter.value_type == "command")
+                        parameter.value_type = 63;
+                }
             });
         });
     });
